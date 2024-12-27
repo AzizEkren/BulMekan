04d19fc5608a38eb544914711c9791fed30eecc1
Aziz Ekren <154012924+AzizEkren@users.noreply.github.com>
2024-12-27 11:35:44 +0300

    Değişim

--- frontend-react-main/src/services/http-common.jsx
+++ frontend-react-main/src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://bul-mekan.vercel.app/",
+  baseURL: "https://bul-mekan-azizs-projects-6db5a937.vercel.app/",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
